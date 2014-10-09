d307c2864a4a1102e8db71d486b9ff26cf7f73f5
suisho <suisho@suisho-no-MacBook-Air.local>
2014-10-09 22:54:49 +0900

    add

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "nsattrchanger" do
+
+end
+
+target "nsattrchangerTests" do
+
+end
+
---
+++ nsattrchanger/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  nsattrchanger
+//
+//  Created by suisho on 2014/10/09.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let label = self.view.viewWithTag(1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

baf8b88b476e4381648a68e3fb258d52d21c41f7
suisho <suisho000000@gmail.com>
2014-10-10 09:44:45 +0900

    gitignored

--- Pods/Pods-nsattrchanger-SDWebImage-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_nsattrchanger_SDWebImage : NSObject
-@end
-@implementation PodsDummy_Pods_nsattrchanger_SDWebImage
-@end
--- Pods/Pods-nsattrchanger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_nsattrchanger : NSObject
-@end
-@implementation PodsDummy_Pods_nsattrchanger
-@end
